665798d840f8715e2b270dc89ce456d1fb4982b3
dinotalic <dinotalic89@gmail.com>
2016-11-01 11:16:35 +0100

    prvi php

---
+++ amel.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sta reci</title>
+</head>
+<body>
+<?php
+    $var1 = 10;
+    echo is_int($var1);
+?>
+</body>
+</html>
